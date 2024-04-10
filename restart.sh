c7a1705c07dfffb9b1e655d71d4554aca6b18c9e
Joey Innes <joey@inneslabs.uk>
2024-04-10 15:49:54 +0200

    Add script

---
+++ restart.sh
@@ -0,0 +1,7 @@
+git pull
+go build
+sudo cp -f dave.service.conf /etc/systemd/system/dave.service
+sudo systemctl daemon-reload
+sudo systemctl enable dave.service
+sudo systemctl stop dave.service
+sudo systemctl start dave.service
