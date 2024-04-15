71241f3686acfb64d6098fd311f5445ecc16fe2d
Joey Innes <joey@inneslabs.uk>
2024-04-15 21:48:35 +0200

    Add license

--- godave/godave.go
+++ godave/godave.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Joey Innes
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package godave
 
 import (
@@ -19,7 +33,7 @@ import (
 const (
 	PERIOD                 = 127713920 * time.Nanosecond // Heartbeat
 	PACKET_SIZE            = 1500                        // Size of buffer
-	VAL_SIZE               = 1280                        // Maximum size of
+	VAL_SIZE               = 1280                        // Size limit of Dat.Val
 	NPEER                  = 3                           // Number of peers to give
 	TOLERANCE              = 2                           // Threshold of peer.nping to increment peer.drop
 	DROP                   = 4                           // Tolerance multiplier
