b2fe891d3bef49bcf395bc4a0d2a0c8cb7c04105
Joey Innes <joey@inneslabs.uk>
2024-04-18 15:03:23 +0200

    y

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 git pull
-/usr/local/go/bin work init godave daved
-cd daved && /usr/local/go/bin build && cd ..
+/usr/local/go/bin/go work init godave daved
+cd daved && /usr/local/go/bin/go build && cd ..
 sudo cp -f dave.service.conf /etc/systemd/system/dave.service
 sudo systemctl daemon-reload
 sudo systemctl enable dave.service
