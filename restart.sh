9a293d2d1de0e8d447e35040fe93875d15a44c71
Joey Innes <joey@inneslabs.uk>
2024-04-21 12:19:26 +0200

    adjust svc conf

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 git pull
-/usr/local/go/bin/go work init godave daved
-cd daved && /usr/local/go/bin/go build && cd ..
+/usr/local/go/bin/go work init godave dapi daved
+cd daved && /usr/local/go/bin/go build -o bin/daved && cd ..
 sudo cp -f dave.service.conf /etc/systemd/system/dave.service
 sudo systemctl daemon-reload
 sudo systemctl enable dave.service
