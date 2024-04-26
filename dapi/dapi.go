0e93c9a5d08ab727d08e137a522920bcb9ed78e7
Joey Innes <joey@inneslabs.uk>
2024-04-26 20:44:39 +0200

    Get cans going

--- dapi/dapi.go
+++ dapi/dapi.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"hash/fnv"
 	"io"
+	"runtime"
 	"time"
 
 	"github.com/intob/dave/dapi/can"
 	"github.com/intob/dave/godave"
 	"github.com/intob/dave/godave/dave"
+	"github.com/intob/jfmt"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -100,13 +102,109 @@ func Walk(d *godave.Dave, work []byte, timeout time.Duration, retry uint) ([]*ca
 	return wc, wd, nil
 }
 
-func PutCan(d *godave.Dave, c *can.Can) error {
-	cb, err := proto.Marshal(c)
-	if err != nil {
-		return err
-	}
-	t := time.Now()
-	m := &dave.M{Op: dave.Op_SET, Val: cb, Time: godave.Ttb(t)}
-	m.Work, m.Nonce = godave.Work(m.Val, m.Time, 3)
-	return SendM(d, m, time.Second)
+func Read(r io.Reader, size int) <-chan []byte {
+	ch := make(chan []byte, 1)
+	go func() {
+		for {
+			buf := make([]byte, size)
+			n, err := r.Read(buf)
+			ch <- buf[:n]
+			if err == io.EOF {
+				break
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func PrepChunks(chunks <-chan []byte, difficulty int) <-chan *dave.M {
+	out := make(chan *dave.M, 1)
+	go func() {
+		for c := range chunks {
+			done := make(chan struct{})
+			go func() {
+				tim := time.NewTicker(time.Second)
+				t := time.Now()
+				for {
+					select {
+					case <-done:
+						fmt.Print("\n")
+						return
+					case <-tim.C:
+						fmt.Printf("\rworking for %s\033[0K", jfmt.FmtDuration(time.Since(t)))
+					}
+				}
+			}()
+			m := &dave.M{Op: dave.Op_SET, Val: c, Time: godave.Ttb(time.Now())}
+			type sol struct{ work, nonce []byte }
+			solch := make(chan sol)
+			ncpu := max(runtime.NumCPU()-2, 1)
+			fmt.Printf("running on %d cores\n", ncpu)
+			for n := 0; n < ncpu; n++ {
+				go func() {
+					w, n := godave.Work(m.Val, m.Time, difficulty)
+					solch <- sol{w, n}
+				}()
+			}
+			s := <-solch
+			m.Work = s.work
+			m.Nonce = s.nonce
+			done <- struct{}{}
+			out <- m
+		}
+		close(out)
+	}()
+	return out
+}
+
+func SendDats(d *godave.Dave, mch <-chan *dave.M) <-chan *dave.M {
+	out := make(chan *dave.M, 1)
+	go func() {
+		for m := range mch {
+			err := SendM(d, m, time.Second)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("sent %x\n", m.Work)
+			out <- m
+		}
+		close(out)
+	}()
+	return out
+}
+
+func MakeCans(d *godave.Dave, difficulty int, mch <-chan *dave.M, ndat, valsize int) <-chan *dave.M {
+	out := make(chan *dave.M, 1)
+	go func() {
+		c := &can.Can{Dats: make([][]byte, 0, ndat)}
+		for {
+			m, ok := <-mch
+			if ok {
+				c.Dats = append(c.Dats, m.Work)
+			}
+			if len(c.Dats) > 0 && (!ok || len(c.Dats) >= ndat) {
+				cb, err := proto.Marshal(c)
+				if err != nil {
+					panic(err)
+				}
+				if len(cb) > valsize {
+					panic("err: can is too big")
+				}
+				cm := &dave.M{Op: dave.Op_SET, Val: cb, Time: godave.Ttb(time.Now())}
+				cm.Work, cm.Nonce = godave.Work(cb, cm.Time, difficulty)
+				err = SendM(d, cm, time.Second)
+				if err != nil {
+					panic(err)
+				}
+				out <- cm
+				c.Dats = make([][]byte, 0, ndat)
+			}
+			if !ok {
+				break
+			}
+		}
+		close(out)
+	}()
+	return out
 }
