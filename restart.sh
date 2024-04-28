b55005760ac1d5e20b03f4a882a230ea7b63ba5e
Joey Innes <joey@inneslabs.uk>
2024-04-28 18:41:53 +0200

    Clean up service config

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 git pull
+sudo cp -f $1 /etc/systemd/system/dave.service
 /usr/local/go/bin/go work init godave dapi daved
 cd daved && /usr/local/go/bin/go build -o bin/daved && cd ..
-sudo cp -f dave_genesis.service.conf /etc/systemd/system/dave.service
 sudo systemctl daemon-reload
 sudo systemctl enable dave.service
 sudo systemctl stop dave.service
