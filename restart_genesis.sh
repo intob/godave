15e90a987d85894830e45f3a82149d680e7a8526
Joey Innes <joey@inneslabs.uk>
2024-04-28 13:25:25 +0200

    Move a lot of bootstrap stuff

--- restart_genesis.sh
+++ restart_genesis.sh
@@ -1,7 +1,7 @@
 git pull
 /usr/local/go/bin/go work init godave dapi daved
 cd daved && /usr/local/go/bin/go build -o bin/daved && cd ..
-sudo cp -f dave.service.conf /etc/systemd/system/dave.service
+sudo cp -f dave_genesis.service.conf /etc/systemd/system/dave.service
 sudo systemctl daemon-reload
 sudo systemctl enable dave.service
 sudo systemctl stop dave.service
---
+++ restart_peer.sh
@@ -0,0 +1,8 @@
+git pull
+/usr/local/go/bin/go work init godave dapi daved
+cd daved && /usr/local/go/bin/go build -o bin/daved && cd ..
+sudo cp -f dave_peer.service.conf /etc/systemd/system/dave.service
+sudo systemctl daemon-reload
+sudo systemctl enable dave.service
+sudo systemctl stop dave.service
+sudo systemctl start dave.service
