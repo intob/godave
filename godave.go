b20c54eaaac4ef6ba807375e0049e44f290d8c79
Joey Innes <joey@inneslabs.uk>
2024-05-04 02:10:13 +0200

    here we go

--- godave.go
+++ godave.go
@@ -1,22 +1,7 @@
 // Copyright 2024 Joey Innes <joey@inneslabs.uk>
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package godave
 
@@ -47,12 +32,12 @@ const (
 	NPEER    = 2
 	EPOCH    = 65537 * time.Nanosecond
 	DELAY    = 5039
-	SHARE    = 257
+	OPEN     = 257
 	PING     = 719
 	DROP     = 1597
 	PRUNE    = 32768
 	SEED     = 2
-	LEECH    = 3
+	PULL     = 3
 	SEEDSEED = 911
 )
 
@@ -177,7 +162,7 @@ func Pdfp(h hash.Hash, pd *dave.Pd) []byte {
 }
 
 func d(c *net.UDPConn, prs map[string]*peer, pch <-chan *pkt, send <-chan *dave.M, recv chan<- *dave.M, log chan<- string, cap int, seed bool) {
-	dats := make(map[uint64]Dat)
+	dats := make(map[uint8]map[uint64]Dat)
 	var nepoch uint64
 	et := time.NewTicker(EPOCH)
 	pdhfn := fnv.New64a()
@@ -185,27 +170,34 @@ func d(c *net.UDPConn, prs map[string]*peer, pch <-chan *pkt, send <-chan *dave.
 		select {
 		case <-et.C: // PERIODICALLY PER EPOCH
 			nepoch++
-			if nepoch%PRUNE == 0 { // PRUNING MEMORY, KEEP <=CAP HEAVIEST DATS
+			if nepoch%PRUNE == 0 { // PRUNING MEMORY, KEEP <=CAP MASSIVE DATS
 				memstat := &runtime.MemStats{}
 				runtime.ReadMemStats(memstat)
-				newdats := make(map[uint64]Dat)
+				newdats := make(map[uint8]map[uint64]Dat)
 				var minmass float64
-				var ld uint64
-				for k, d := range dats {
-					mass := Mass(d.W, d.Ti)
-					if len(newdats) >= cap-1 { // BEYOND CAP, REPLACE BY WEIGHT
-						if mass > minmass {
-							delete(newdats, ld)
-							lg(log, "/d/prune/delete %d with weight %f\n", ld, minmass)
-							newdats[k] = d
-							ld = k
-							minmass = mass
+				var ld, count uint64
+				for shardid, shard := range dats {
+					for key, dat := range shard {
+						count++
+						mass := Mass(dat.W, dat.Ti)
+						if len(shard) >= cap-1 { // BEYOND CAP, REPLACE BY MASS
+							if mass > minmass {
+								delete(newdats[shardid], ld)
+								lg(log, "/d/prune/delete %d with weight %f\n", ld, minmass)
+								newdats[shardid][key] = dat
+								ld = key
+								minmass = mass
+							}
+						} else {
+							if mass < minmass {
+								minmass = mass
+							}
+							_, ok := newdats[shardid]
+							if !ok {
+								newdats[shardid] = make(map[uint64]Dat)
+							}
+							newdats[shardid][key] = dat
 						}
-					} else {
-						if mass < minmass {
-							minmass = mass
-						}
-						newdats[k] = d
 					}
 				}
 				dats = newdats
@@ -214,30 +206,33 @@ func d(c *net.UDPConn, prs map[string]*peer, pch <-chan *pkt, send <-chan *dave.
 					newpeers[k] = p
 				}
 				prs = newpeers
-				lg(log, "/d/prune/keep %d peers, %d dats, %.2fMB mem alloc\n", len(newpeers), len(newdats), float32(memstat.Alloc)/1024/1024)
+				lg(log, "/d/prune/keep %d peers, %d dats across %d shards, %.2fMB mem alloc\n", len(newpeers), count, len(newdats), float32(memstat.Alloc)/1024/1024)
 			}
-			if (!seed || nepoch%SEEDSEED == 0) && len(dats) > 0 && len(prs) > 0 { // SEND RANDOM DAT TO RANDOM PEER, SEEDS MAY PRIORITISE PEER MESSAGES
-				rdati := mrand.Intn(len(dats))
-				var x int
-				for s := range dats {
-					if x == rdati {
-						rd := dats[s]
-						m := marshal(&dave.M{Op: dave.Op_DAT, Val: rd.V, Time: Ttb(rd.Ti), Salt: rd.S, Work: rd.W})
-						for _, rp := range randpds(prs, nil, 1, usable) {
-							wraddr(c, m, addrfrom(rp))
-							lg(log, "/d/rand sent to %x %s\n", Pdfp(pdhfn, rp), rd.V)
+			if ((seed && nepoch%SEEDSEED == 0) || (!seed && nepoch%SEED == 0)) && len(dats) > 0 && len(prs) > 0 { // SEND RANDOM DAT TO RANDOM PEER, SEEDS MAY PRIORITISE PEER MESSAGES
+				rshard := uint8(mrand.Intn(256))
+				if len(dats[rshard]) > 0 {
+					rindex := mrand.Intn(len(dats[rshard]))
+					var x int
+					for s := range dats[rshard] {
+						if x == rindex {
+							rd := dats[rshard][s]
+							m := marshal(&dave.M{Op: dave.Op_DAT, V: rd.V, T: Ttb(rd.Ti), S: rd.S, W: rd.W})
+							for _, rp := range randpds(prs, nil, 1, usable) {
+								wraddr(c, m, addrfrom(rp))
+								lg(log, "/d/rand sent to %x %s\n", Pdfp(pdhfn, rp), rd.V)
+							}
+							break
 						}
-						break
+						x++
 					}
-					x++
 				}
 			}
-			if nepoch%SHARE == 0 { // ONCE PER SHARE EPOCHS, RELATIVELY SHORT CYCLE
+			if nepoch%OPEN == 0 { // ONCE PER SHARE EPOCHS, RELATIVELY SHORT CYCLE
 				for pid, p := range prs {
 					if !p.bootstrap && time.Since(p.seen) > EPOCH*DROP { // DROP UNRESPONSIVE PEER
 						delete(prs, pid)
 						lg(log, "/d/peer/remove %x\n", Pdfp(pdhfn, p.pd))
-					} else if time.Since(p.seen) > EPOCH*SHARE && time.Since(p.ping) > EPOCH*PING { // SEND PING
+					} else if time.Since(p.seen) > EPOCH*OPEN && time.Since(p.ping) > EPOCH*PING { // SEND PING
 						wraddr(c, marshal(&dave.M{Op: dave.Op_GETPEER}), addrfrom(p.pd))
 						p.ping = time.Now()
 						lg(log, "/d/peer/ping sent GETPEER to %x\n", Pdfp(pdhfn, p.pd))
@@ -248,7 +243,7 @@ func d(c *net.UDPConn, prs map[string]*peer, pch <-chan *pkt, send <-chan *dave.
 			if m != nil {
 				switch m.Op {
 				case dave.Op_DAT:
-					store(dats, &Dat{m.Val, m.Salt, m.Work, Btt(m.Time)})
+					store(dats, &Dat{m.V, m.S, m.W, Btt(m.T)})
 					for _, rp := range randpds(prs, nil, 1, usable) {
 						wraddr(c, marshal(m), addrfrom(rp))
 						lg(log, "/d/send DAT to %x\n", Pdfp(pdhfn, rp))
@@ -282,20 +277,20 @@ func d(c *net.UDPConn, prs map[string]*peer, pch <-chan *pkt, send <-chan *dave.
 				randpds := randpds(prs, []*dave.Pd{pktpd}, NPEER, usable)
 				wraddr(c, marshal(&dave.M{Op: dave.Op_PEER, Pds: randpds}), pkt.ip)
 				lg(log, "/d/ph/getpeer/reply with PEER to %x\n", Pdfp(pdhfn, pktpd))
-			case dave.Op_DAT: // CHECK WORK, THEN STORE DAT OR SERVE SHAGET
-				check := Check(m.Val, m.Time, m.Salt, m.Work)
-				if check == -1 { // BYTES DON'T MATCH, MAYBE IT'S A SHAGET
-					d, ok := dats[id(m.Work)]
-					if ok {
-						wraddr(c, marshal(&dave.M{Op: dave.Op_DAT, Val: d.V, Time: Ttb(d.Ti), Salt: d.S, Work: d.W}), pkt.ip)
-						lg(log, "/d/ph/shaget/reply with DAT\n")
-					}
-				}
-				store(dats, &Dat{m.Val, m.Salt, m.Work, Btt(m.Time)})
-				lg(log, "/d/ph/dat/store %x\n", m.Work)
+			case dave.Op_DAT: // STORE DAT
+				store(dats, &Dat{m.V, m.S, m.W, Btt(m.T)})
+				lg(log, "/d/ph/dat/store %x\n", m.W)
 			}
 		}
 	}
+}
+
+func store(dats map[uint8]map[uint64]Dat, d *Dat) {
+	_, ok := dats[uint8(d.W[31])]
+	if !ok {
+		dats[uint8(d.W[31])] = make(map[uint64]Dat)
+	}
+	dats[uint8(d.W[31])][id(d.W)] = *d
 }
 
 func lstn(c *net.UDPConn, fcap uint, log chan<- string) <-chan *pkt {
@@ -352,8 +347,11 @@ func rdpkt(c *net.UDPConn, f *ckoo.Filter, h hash.Hash, bufpool, mpool *sync.Poo
 	if m.Op == dave.Op_PEER && len(m.Pds) > NPEER {
 		lg(log, "/rdpkt/drop/npeer too many peers\n")
 		return nil
+	} else if m.Op == dave.Op_DAT && Check(m.V, m.T, m.S, m.W) < 0 {
+		lg(log, "/rdpkt/drop/workcheck invalid\n")
+		return nil
 	}
-	cpy := &dave.M{Op: m.Op, Pds: make([]*dave.Pd, len(m.Pds)), Val: m.Val, Time: m.Time, Salt: m.Salt, Work: m.Work}
+	cpy := &dave.M{Op: m.Op, Pds: make([]*dave.Pd, len(m.Pds)), V: m.V, T: m.T, S: m.S, W: m.W}
 	for i, pd := range m.Pds {
 		cpy.Pds[i] = &dave.Pd{Ip: pd.Ip, Port: pd.Port}
 	}
@@ -385,7 +383,7 @@ func randpds(prs map[string]*peer, excl []*dave.Pd, lim int, match func(*peer) b
 }
 
 func usable(k *peer) bool {
-	return k.bootstrap || time.Since(k.seen) < EPOCH*SHARE && time.Since(k.added) > EPOCH*DELAY
+	return k.bootstrap || time.Since(k.seen) < EPOCH*OPEN && time.Since(k.added) > EPOCH*DELAY
 }
 
 func addrfrom(pd *dave.Pd) netip.AddrPort {
@@ -433,10 +431,6 @@ func nzero(key []byte) int {
 		}
 	}
 	return len(key)
-}
-
-func store(dats map[uint64]Dat, dat *Dat) {
-	dats[id(dat.W)] = *dat
 }
 
 func lg(ch chan<- string, msg string, args ...any) {
